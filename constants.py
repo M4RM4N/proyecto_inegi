3f165d59826b54380697b0f87431872de4bc9c44
Edwin Jaziel Franco Arellano <77560075+Franco030@users.noreply.github.com>
2025-11-04 16:40:12 -0600

    Update constants.py

--- constants.py
+++ constants.py
@@ -4,10 +4,11 @@ from sqlalchemy import create_engine
 # You can define the connection string as a constant
 DB_CONNECTION_STRING = "mysql+pymysql://franquito:edwin@localhost:3306/inegi"
 ASSISTANT_CONNECTION_STRING = "mysql+pymysql://assistant:edwin@localhost:3306/inegi"
-GEMINI_API_KEY = "AIzaSyAFH3PjHBDT1zltBDtjH0nSZgOWdckrXDc"
+GEMINI_API_KEY = ""
 
 
 # You can define the engine object itself as a constant
 # We create it here so it's initialized only once when the module is imported.
 # Note: setting echo=True is often helpful for debugging SQL
+
 ENGINE = create_engine(DB_CONNECTION_STRING, echo=True)
